26f2e86c53fa14dadb69a09a4c7efe4d0a7efe20
Wulliom <maarten.vandeelen@novi-education.nl>
2023-07-21 11:29:38 +0200

    test

---
+++ src/main/java/com/example/testprojectgit/TestprojectgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprojectgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojectgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestprojectgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testprojectgit/TestprojectgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprojectgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
